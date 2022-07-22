ae024385a8e8818838c970ef29891d23447f9b43
ChanYeongL <chanyeonglee@kaist.ac.kr>
2022-07-22 09:52:26 +0900

    first commit

---
+++ Energy_Balance_integrated.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+import netCDF4
+
+file_location = input("Insert location of balance.nc file : ")
+nc_balance = netCDF4.Dataset(file_location)
+
+fhi_32      = nc_balance['fhi_32'][:]
+fhi_52      = nc_balance['fhi_52'][:]
+fhi_cond    = nc_balance['fhi_cond'][:]
+fhi_dia     = nc_balance['fhi_dia'][:]
+fhi_ecrb    = nc_balance['fhi_ecrb'][:]
+fhi_strange = nc_balance['fhi_strange'][:]
+fhi_pschused= nc_balance['fhi_pschused'][:]
+fhi_inert   = nc_balance['fhi_inert'][:]
+fhi_vispar  = nc_balance['fhi_vispar'][:]
+fhi_visper  = nc_balance['fhi_visper'][:]
+fhi_visq    = nc_balance['fhi_visq'][:]
+fhi_anml    = nc_balance['fhi_anml'][:]
+fhi_kevis   = nc_balance['fhi_kevis'][:]
+
+fhe_32      = nc_balance['fhe_32'][:]
+fhe_52      = nc_balance['fhe_52'][:]
+fhe_thermj   = nc_balance['fhe_thermj'][:]
+fhe_cond    = nc_balance['fhe_cond'][:]
+fhe_dia     = nc_balance['fhe_dia'][:]
+fhe_ecrb    = nc_balance['fhe_ecrb'][:]
+fhe_strange = nc_balance['fhe_strange'][:]
+fhe_pschused= nc_balance['fhe_pschused'][:]
+#fhe_vispar  = nc_balance['fhe_vispar'][:]
+#fhe_visper  = nc_balance['fhe_visper'][:]
+#fhe_visq    = nc_balance['fhe_visq'][:]
+#fhe_anml    = nc_balance['fhe_anml'][:]
+#fhe_kevis   = nc_balance['fhe_kevis'][:]
+
+total_fhi =fhi_32+fhi_52+fhi_cond+fhi_dia+fhi_ecrb+fhi_strange+fhi_pschused+fhi_inert+fhi_vispar+fhi_visper+fhi_visq+fhi_anml+fhi_kevis
+total_fhe =fhe_32+fhe_52+fhe_cond+fhe_dia+fhe_ecrb+fhe_strange+fhe_pschused+fhe_thermj
+
+eirene_mc_eael_she_bal = nc_balance['eirene_mc_eael_she_bal'][:]
+eirene_mc_emel_she_bal = nc_balance['eirene_mc_emel_she_bal'][:]
+eirene_mc_eiel_she_bal = nc_balance['eirene_mc_eiel_she_bal'][:]
+eirene_mc_epel_she_bal = nc_balance['eirene_mc_epel_she_bal'][:]
+
+eirene_mc_eapl_shi_bal = nc_balance['eirene_mc_eapl_shi_bal'][:]
+eirene_mc_empl_shi_bal = nc_balance['eirene_mc_empl_shi_bal'][:]
+eirene_mc_eipl_shi_bal = nc_balance['eirene_mc_eipl_shi_bal'][:]
+eirene_mc_eppl_shi_bal = nc_balance['eirene_mc_eppl_shi_bal'][:]
+
+
+
+
+
+def heat_flux(valname):
+    total_heat_loc = 1000000
+    fht_local = valname
+
+    heat_outer_diver = np.sum((fht_local[1,1:,96]))/total_heat_loc+ np.sum((fht_local[0,1:,96]))/total_heat_loc
+
+    heat_inner_diver = np.sum((fht_local[1,1:,1]))/total_heat_loc+np.sum((fht_local[0,1:,1]))/total_heat_loc
+
+    heat_PRF = (np.sum((fht_local[1,1,77:]))+np.sum((fht_local[1,1,:11])))/total_heat_loc
+
+    sol_heat = (np.sum(fht_local[0,37,:])+np.sum(fht_local[1,37,:]))/total_heat_loc
+
+    inner_heat = (np.sum((fht_local[0,0,11:77]))+np.sum((fht_local[1,0,11:77])))/total_heat_loc
+    
+    return -heat_outer_diver,heat_inner_diver,heat_PRF,-sol_heat,inner_heat
+
+def eirene_sum():
+    sum_eirene_mc_eael_she_bal = np.sum(eirene_mc_eael_she_bal)
+    sum_eirene_mc_emel_she_bal = np.sum(eirene_mc_emel_she_bal)
+    sum_eirene_mc_eiel_she_bal = np.sum(eirene_mc_eiel_she_bal)
+    sum_eirene_mc_epel_she_bal = np.sum(eirene_mc_epel_she_bal)
+
+    sum_eirene_mc_eapl_shi_bal = np.sum(eirene_mc_eapl_shi_bal)
+    sum_eirene_mc_empl_shi_bal = np.sum(eirene_mc_empl_shi_bal)
+    sum_eirene_mc_eipl_shi_bal = np.sum(eirene_mc_eipl_shi_bal)
+    sum_eirene_mc_eppl_shi_bal = np.sum(eirene_mc_eppl_shi_bal)
+    
+    return (sum_eirene_mc_eael_she_bal+sum_eirene_mc_emel_she_bal+sum_eirene_mc_eiel_she_bal+sum_eirene_mc_epel_she_bal +sum_eirene_mc_eapl_shi_bal+sum_eirene_mc_eapl_shi_bal+sum_eirene_mc_eapl_shi_bal+sum_eirene_mc_eapl_shi_bal)/1000000
+
+def other_source():
+    heat_source= np.sum(nc_balance['b2srst_shi_bal'][:])+np.sum(nc_balance['b2srst_she_bal'][:])
+    stel_terms = np.sum(nc_balance['b2stel_shi_ion_bal'][:])+np.sum(nc_balance['b2stel_she_bal'][:])
+    
+    joule = np.sum(nc_balance['b2sihs_joule_bal'][:])
+
+    fraa  = np.sum(nc_balance['b2sihs_fraa_bal'][:])
+    
+    divue = np.sum(nc_balance['b2sihs_divue_bal'][:])
+    
+    visa  = np.sum(nc_balance['b2sihs_visa_bal'][:])
+    return (heat_source+stel_terms+joule+fraa+divue+visa)/1000000
+
+print(sum(heat_flux(total_fhe)))
+print(sum(heat_flux(total_fhi)))
+
+print('total internal energy "outward" = ',-(sum(heat_flux(total_fhe))+sum(heat_flux(total_fhi))+eirene_sum()+other_source()))
+
+plt.title("Ion heat flux(+ sign means outward energy)")
+x = [r'$P_{outer div}$',r'$P_{inner div}$',r'$P_{PFR}$',r'P_{SOL}',r'$P_{core}$']
+plt.bar(x,heat_flux(-total_fhi))
+plt.axhline(color = 'black',linewidth = 0.5)
+plt.show()
+
+plt.title("Electron heat flux(+ sign means outward energy)")
+x = [r'$P_{outer div}$',r'$P_{inner div}$',r'$P_{PFR}$',r'P_{SOL}',r'$P_{core}$']
+plt.bar(x,heat_flux(-total_fhe))
+plt.axhline(color = 'black',linewidth = 0.5)
+plt.show()
+
+plt.title("Energy Balance(total input heat 2.5 MW)")
+x = [r'$P_{ion}$',r'$P_{electron}$',r'$P_{eirene}$',r'P_{etc}']
+y = [-sum(heat_flux(total_fhe)),-sum(heat_flux(total_fhi)),-eirene_sum(),-other_source()]
+plt.bar(x,y)
+plt.axhline(color = 'black',linewidth = 0.5)
+
+
+plt.show()
---
+++ integrated_data.py
@@ -0,0 +1,310 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pickle
+import os, sys
+import paramiko
+import getpass
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+#ssh.connect("143.248.99.71",username="chanyeong",password="satirev147")
+#print('ssh connected.')
+
+
+#sftp = ssh.open_sftp()
+#sftp.get('/home/chanyeong/solps-iter/runs/Exp_ne_zero/baserun/b2.neutrals.parameters.stencil', './holyshit.txt')
+
+
+#delete = "rm ./b2fplasmf_trial.txt"
+
+puff = "noNe"
+
+ws_name = input("Choose workstation(e.g ws4) ")
+
+ID = input("Insert I.D : ")
+
+pw = getpass.getpass("Insert Password : ")
+
+mkdir_bool = input("would you make another directory?(y/n) ")
+
+if mkdir_bool == "y":
+	output_dir = input("Insert a name of the new output directory : ")
+	os.mkdir(output_dir)
+elif mkdir_bool == "n":
+	output_dir = input("Choose a output directory : ")
+
+else:
+	print("Invalid input")
+	sys.exit(0)
+	
+
+if ws_name=='ws1':
+	ssh.connect("143.248.98.209",username="%s" %ID,password="%s" %pw)
+	print('ws1 connected.')
+elif ws_name=='ws2':
+	ssh.connect("143.248.99.155",username="%s" %ID,password="%s" %pw)
+	print('ws2 connected.')
+elif ws_name=='ws3':
+	ssh.connect("143.248.98.247",username="%s" %ID,password="%s"%pw)
+	print('ssh ws3 connected.')
+elif ws_name=='ws4':
+	ssh.connect("143.248.99.71",username="%s" %ID,password="%s" %pw)
+	print('ssh ws4 connected.')
+elif ws_name=='ws5':
+	ssh.connect("143.248.101.40",username="%s" %ID,password="%s" %pw)
+	print('ssh ws5 connected.')
+
+sftp = ssh.open_sftp()
+
+
+#ws4
+if ws_name=='ws4':
+	fname_list = list()
+	fpath_list = list()
+#	fname_list.append("b2fplasmf_al_KSTAR_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/altergeo_sput_Ne_zero/Baseline_KSTAR/b2fplasmf")
+#	fname_list.append("b2fplasmf_al_ITER_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/altergeo_sput_Ne_zero/ITER_Heating/b2fplasmf")
+#	fname_list.append("b2fplasmf_al_KDEMO_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/altergeo_sput_Ne_zero/KDEMO_Heating")
+#	fname_list.append("b2fplasmf_co_KDEMO_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_no_neon/KDEMO_Heating")
+
+#fname_list.append("b2fstate_al_3e7P_%s" %puff)
+
+#	fname_list.append("b2fplasmf_co_KSTAR_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_no_neon/Baseline_KSTAR/b2fplasmf")
+#	fname_list.append("b2fplasmf_co_ITER_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_no_neon/ITER_Heating/b2fplasmf")
+#	fname_list.append("b2fplasmf_co_32MW_%s" %puff)
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_Ne_3e19/1.6e17Heating/b2fplasmf")a
+
+#	fname_list.append("b2fplasmf_SAS_32MW_Ne_zero")
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/SAS_Ne_zero/32MW_Heating/b2fplasmf")
+
+#	fname_list.append("b2fplasmf_co_32MW_Ne_zero")
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/con_Ne_zero/32MW_Heating/b2fplasmf")
+
+#	fname_list.append("b2fplasmf_co_32MW_Ne_3e19")
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/con_Ne_3e19/32MW_Heating/b2fplasmf")
+
+
+	fname_list.append("b2fplasmf_new_mesh_al_Ne_zero")
+	fpath_list.append("/home/chanyeong/solps-iter/runs/SAS_Ne_zero/Baseline_KSTAR")
+
+	fname_list.append("b2fplasmf_co_Ne_zero")
+	fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_no_neon/Baseline_KSTAR")
+
+#	fname_list.append("b2fplasmf_co_32MW_Ne_3e19")
+#	fpath_list.append("/home/chanyeong/solps-iter/runs/con_Ne_3e19/32MW_Heating/b2fplasmf")
+
+
+
+#Arbirary name
+#fname_list.append("b2fplasmf_al_1.6e7_3e16")
+#fpath_list.append("/home/chanyeong/solps-iter/runs/altergeo_sput_neon/1.6e7Heating/b2fplasmf")
+#fname_list.append("b2fplasmf_co_1.6e7_3e16")
+#fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_sput_neon/1.6e7Heating/b2fplasmf")
+#fname_list.append("b2fplasmf_co_1.6e7_3e19")
+#fpath_list.append("/home/chanyeong/solps-iter/runs/conventionalgeo_Ne_3e19/1.6e17Heating/b2fplasmf")
+
+#densi_list = list()
+#densi_list.append("na")
+#densi_list.append("ne")
+
+	#ws3
+elif ws_name=='ws3':
+	fname_list.append("b2fplasmf_co_KSTAR_%s" %puff)
+	fpath_list.append("home/chanyeong/solps-iter/runs/convgeo_Ne_3e16/Baseline_KSTAR/b2fplasmf")
+
+data_list = list()
+data_list.append("fht")
+data_list.append("fhi")
+data_list.append("fhe")
+data_list.append("rqrad")
+data_list.append("na")
+data_list.append("ne")
+data_list.append("ti")
+data_list.append("te")
+
+data_list.append("rrana")
+data_list.append("rrahi")
+data_list.append("rcxhi")
+data_list.append("rsana")
+data_list.append("rsahi")
+data_list.append("po")
+
+
+nx = 96
+ny = 36
+ns = 20
+
+def find_ni(fname):
+    with open(fname) as f:
+        line = f.readlines()
+        print(line)
+    return line
+
+
+for data_ran in range(len(fname_list)):
+	dum_name = fname_list[data_ran]
+#	file = "scp chanyeong@143.248.99.71:/%s ./b2fplasmf_trial.txt" %fpath_list[data_ran] #ws4
+
+	fname =  "./%s.txt" %fname_list[data_ran]
+	file = sftp.get('/%s/b2fplasmf'%fpath_list[data_ran], './%s' %fname)
+	sftp.get('/%s/fort.44'%fpath_list[data_ran], './%s/fort44_%s' %(output_dir, fname_list[data_ran]))
+
+#	file = "scp chanyeong@143.248.98.247:/%s ./b2fplasmf_trial.txt" %fpath_list[data_ran] #ws3
+
+#	os.system(file)
+#fname =  fname_list[data_ran]
+
+	with open(fname,'r') as f:
+		line = f.readlines()    
+	a=type(line)
+	k = len(line)
+	data_array = np.array(line)
+	full_length = np.size(data_array)
+
+	for var_ran in range(len(data_list)):
+		data_name = data_list[var_ran]
+#print(line[0])
+#mesh = line[2].split()
+#nx = int(mesh[0])
+#ny = int(mesh[1])
+#ns = int(mesh[2])
+
+#		print(line[2])
+
+		raw_data = np.empty((full_length,6))
+
+		for i in range(0, full_length):
+
+			dum = line[i].split()
+			dum2 = len(dum)
+
+			for j in range(6):
+				if j+1<=len(dum):
+					dum3 = dum[j]
+
+					try:
+						raw_data[i,j] = dum3
+					except ValueError as m:
+						raw_data[i,j] = 0
+                    #print(dum3)
+				elif j+1>len(dum):
+					continue
+
+
+
+		for i in range(0, full_length):  
+			dum = line[i].split()
+			dum2 = len(dum)
+			for j in range(6):
+				if j+1<=len(dum):
+					dum3 = dum[j]
+					try:
+						raw_data[i,j] = dum3
+					except ValueError as m:
+						raw_data[i,j] = 0
+                        #print(line[i])
+						break;
+				elif j+1>len(dum):
+					continue
+
+		check_array = np.empty((1,6))
+		for ch in range(full_length):
+			if data_name in line[ch]:
+				if line[ch].split()[3]==data_name:
+					position_start = ch+1
+					break;
+				else:
+					continue
+			else:
+				continue
+
+		for ch2 in range(position_start+1,full_length):
+			try:
+				check_cf = line[ch2].split()
+				check_er = float(check_cf[0])
+			except ValueError as m:
+				position_end = ch2-1
+        #        print(ch2)
+				break;
+
+        #print(line[position_end])
+		check_length = line[position_start-1].split()
+		tot_len =int(check_length[2])
+
+
+
+
+		line[250193].split()[3] == data_name
+
+
+		selected_length = position_end-position_start
+		selected_data = np.zeros((selected_length+1,6))
+
+		for i in range(position_start, position_end+1):
+			dum = line[i].split()
+            #print(line[i])
+			dum2 = len(dum)
+			for j in range(dum2):
+				dum3 = dum[j]
+				selected_data[i-position_start,j] = dum3    
+
+        #    for j in range(6):
+        #        if j+1<=len(dum):
+        #            dum3 = dum[j]
+        #            try:
+        #                selected_data[i-position_start,j] = dum3
+        #            except ValueError as m:
+        #                selected_data[i-position_start,j] = 0.0
+        #            #print(dum3)
+        #        elif j+1>len(dum):
+        #            continue
+
+
+
+		numerical_len = np.shape(selected_data)[0]*np.shape(selected_data)[1]
+
+		np.shape(selected_data)
+		selected_data_merged = selected_data.reshape(numerical_len,1)
+		selected_data_merged_deleted = selected_data_merged[:tot_len,0]
+
+
+		if check_length[2] == '3724':
+			selected_data_final = selected_data_merged_deleted.reshape(ny+2,nx+2)
+		elif check_length[2]== '7448':
+			selected_data_final = selected_data_merged_deleted.reshape(2, ny+2,nx+2)
+		else:
+			selected_data_final = selected_data_merged_deleted.reshape(ns, ny+2,nx+2)
+
+		np.save("./%s/%s_%s" %(output_dir,data_name,dum_name), selected_data_final)
+		print("%s_%s was saved" %(data_name,dum_name))
+
+        #selected_data_final = selected_data_merged_deleted.reshape(ns, ny+2,nx+2)
+
+        #print(selected_data_final[0,:,3])
+
+        #selected_data_final[0,:,96]
+
+        #map_plot = plt.pcolormesh(selected_data_final[1,:,:])
+        #plt.savefig(data_name)
+        #plt.title(data_name)
+        #plt.colorbar()
+        #plt.show()
+
+		x_axis = np.linspace(1,98,98)
+		y_axis = np.linspace(1,38,38)
+			
+	delete = "rm %s" %fname
+	os.system(delete)
+ssh.close()
+
+
+
+
+
+
