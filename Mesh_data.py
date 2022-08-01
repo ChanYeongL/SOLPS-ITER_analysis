6cce5bae2cf6508995e9d1c8a7d770c157d13599
ChanYeongL <chanyeonglee@kaist.ac.kr>
2022-08-01 16:32:00 +0900

    some data file

---
+++ Mesh_data.py
@@ -0,0 +1,317 @@
+from operator import ne
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from scipy.special import rel_entr
+import pickle
+from mpl_toolkits.mplot3d import axes3d
+from scipy.ndimage.filters import gaussian_filter
+import seaborn as sns
+import matplotlib.patches as patches
+import pandas as pd
+import netCDF4
+
+
+
+ny = 36
+nx = 96
+
+co_mesh = np.load("./conv_geo_mesh_center.npy")
+co_mesh_x = np.load("./conv_geo_mesh_x.npy")
+co_mesh_y = np.load("./conv_geo_mesh_y.npy")
+
+al_mesh = np.load("./al_geo_mesh_center.npy")
+al_mesh_x = np.load("./al_geo_mesh_x.npy")
+al_mesh_y = np.load("./al_geo_mesh_y.npy")
+
+co_facing = np.load("./gs_b2fgmtry.npy")
+al_facing = np.load("./gs_b2fgmtry_al.npy")
+
+
+co_new_mesh = np.load("./new_mesh_co_geo_mesh_center.npy")
+co_new_mesh_x = np.load("./new_mesh_co_geo_mesh_x.npy")
+co_new_mesh_y = np.load("./new_mesh_co_geo_mesh_y.npy")
+co_newmesh_facing = np.load("./gs_new_mesh_co.npy")
+
+
+al_new_mesh = np.load("./new_mesh_al_geo_mesh_center.npy")
+al_new_mesh_x = np.load("./new_mesh_al_geo_mesh_x.npy")
+al_new_mesh_y = np.load("./new_mesh_al_geo_mesh_y.npy")
+al_newmesh_facing = np.load("./gs_new_mesh_al.npy")
+
+
+newSAS_mesh = np.load("./new_SAS_geo_mesh_center.npy")
+newSAS_mesh_x = np.load("./new_SAS_geo_mesh_y.npy")
+newSAS_mesh_y = np.load("./new_SAS_geo_mesh_y.npy")
+newSAS_facing = np.load("./gs_new_SAS.npy")
+
+
+
+def co_distance_center(first_point, second_point):
+    x_range = x_co_meshinfo[first_point[1],first_point[0]]-x_co_meshinfo[second_point[1],second_point[0]]
+    y_range = y_co_meshinfo[first_point[1],first_point[0]]-y_co_meshinfo[second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def co_distance_point(first_point, second_point):
+    x_range = co_mesh_x[first_point[2],first_point[1],first_point[0]]-co_mesh_x[second_point[2],second_point[1],second_point[0]]
+    y_range = co_mesh_y[first_point[2],first_point[1],first_point[0]]-co_mesh_y[second_point[2],second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def al_distance_center(first_point, second_point):
+    x_range = x_al_meshinfo[first_point[1],first_point[0]]-x_al_meshinfo[second_point[1],second_point[0]]
+    y_range = y_al_meshinfo[first_point[1],first_point[0]]-y_al_meshinfo[second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def al_distance_point(first_point, second_point):
+    x_range = al_mesh_x[first_point[2],first_point[1],first_point[0]]-al_mesh_x[second_point[2],second_point[1],second_point[0]]
+    y_range = al_mesh_y[first_point[2],first_point[1],first_point[0]]-al_mesh_y[second_point[2],second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+    #new geo files
+def co_new_mesh_distance_center(first_point, second_point):
+    x_range = x_co_new_meshinfo[first_point[1],first_point[0]]-x_co_new_meshinfo[second_point[1],second_point[0]]
+    y_range = y_co_new_meshinfo[first_point[1],first_point[0]]-y_co_new_meshinfo[second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+def co_new_mesh_distance_point(first_point, second_point):
+    x_range = co_new_mesh_x[first_point[2],first_point[1],first_point[0]]-co_new_mesh_x[second_point[2],second_point[1],second_point[0]]
+    y_range = co_new_mesh_y[first_point[2],first_point[1],first_point[0]]-co_new_mesh_y[second_point[2],second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+
+def al_new_mesh_distance_center(first_point, second_point):
+    x_range = x_al_new_meshinfo[first_point[1],first_point[0]]-x_al_new_meshinfo[second_point[1],second_point[0]]
+    y_range = y_al_new_meshinfo[first_point[1],first_point[0]]-y_al_new_meshinfo[second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def al_new_mesh_distance_point(first_point, second_point):
+    x_range = al_new_mesh_x[first_point[2],first_point[1],first_point[0]]-al_new_mesh_x[second_point[2],second_point[1],second_point[0]]
+    y_range = al_new_mesh_y[first_point[2],first_point[1],first_point[0]]-al_new_mesh_y[second_point[2],second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def newSAS_distance_center(first_point, second_point):
+    x_range = x_newSAS_meshinfo[first_point[1],first_point[0]]-x_newSAS_meshinfo[second_point[1],second_point[0]]
+    y_range = y_newSAS_meshinfo[first_point[1],first_point[0]]-y_newSAS_meshinfo[second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+def newSAS_distance_point(first_point, second_point):
+    x_range = newSAS_mesh_x[first_point[2],first_point[1],first_point[0]]-newSAS_mesh_x[second_point[2],second_point[1],second_point[0]]
+    y_range = newSAS_mesh_y[first_point[2],first_point[1],first_point[0]]-newSAS_mesh_y[second_point[2],second_point[1],second_point[0]]
+    result = (x_range**2 + y_range**2)**(1/2)
+    return result
+
+
+def total_pressure(ne,te,na,ti):
+    pr = ne[:,:]*te[:,:] + na[1,:,:]*ti[:,:] +np.sum(na[2:9,:,:]*ti[:,:],axis = 0)+np.sum(na[9:,:,:]*ti[:,:],axis = 0)
+    return pr
+
+def neutral_pressure(na,ti):
+    pr = na[0,:,:]*ti[:,:]+ na[0,:,:]*ti[:,:]+ na[2,:,:]*ti[:,:]+na[2,:,:]*ti[:,:]+na[9,:,:]*ti[:,:]
+    return pr
+    
+
+dumx_co_meshinfo = co_mesh[:,0]
+dumy_co_meshinfo = co_mesh[:,1]
+x_co_meshinfo = dumx_co_meshinfo.reshape(ny+2,nx+2)
+y_co_meshinfo = dumy_co_meshinfo.reshape(ny+2,nx+2)
+
+dumx_al_meshinfo = al_mesh[:,0]
+dumy_al_meshinfo = al_mesh[:,1]
+x_al_meshinfo = dumx_al_meshinfo.reshape(ny+2,nx+2)
+y_al_meshinfo = dumy_al_meshinfo.reshape(ny+2,nx+2)
+
+dumx_co_new_meshinfo = co_new_mesh[:,0]
+dumy_co_new_meshinfo = co_new_mesh[:,1]
+x_co_new_meshinfo = dumx_co_new_meshinfo.reshape(ny+2,nx+2)
+y_co_new_meshinfo = dumy_co_new_meshinfo.reshape(ny+2,nx+2)
+
+dumx_al_new_meshinfo = al_new_mesh[:,0]
+dumy_al_new_meshinfo = al_new_mesh[:,1]
+x_al_new_meshinfo = dumx_al_new_meshinfo.reshape(ny+2,nx+2)
+y_al_new_meshinfo = dumy_al_new_meshinfo.reshape(ny+2,nx+2)
+
+dumx_newSAS_meshinfo = newSAS_mesh[:,0]
+dumy_newSAS_meshinfo = newSAS_mesh[:,1]
+x_newSAS_meshinfo = dumx_newSAS_meshinfo.reshape(ny+2,nx+2)
+y_newSAS_meshinfo = dumy_newSAS_meshinfo.reshape(ny+2,nx+2)
+
+
+#po_dumx_meshinfo = convmesh[:,0]
+#po_dumy_meshinfo = convmesh[:,1]
+#po_x_meshinfo = dumx_meshinfo.reshape(4,ny+2,nx+2)
+#po_y_meshinfo = dumy_meshinfo.reshape(4,ny+2,nx+2)
+co_sep_out_target = np.zeros(2)
+co_sep_out_target[0] = co_mesh_x[1,18,96]
+co_sep_out_target[1] = co_mesh_y[1,18,96]
+co_mesh_x_cen = np.sum(co_mesh_x, axis = 0)/4
+co_mesh_y_cen = np.sum(co_mesh_y, axis = 0)/4
+
+al_sep_out_target = np.zeros(2)
+al_sep_out_target[0] = al_mesh_x[1,18,96]
+al_sep_out_target[1] = al_mesh_y[1,18,96]
+al_mesh_x_cen = np.sum(al_mesh_x, axis = 0)/4
+al_mesh_y_cen = np.sum(al_mesh_y, axis = 0)/4
+
+co_new_mesh_sep_out_target = np.zeros(2)
+co_new_mesh_sep_out_target[0] = co_mesh_x[1,18,96]
+co_new_mesh_sep_out_target[1] = co_mesh_y[1,18,96]
+co_new_mesh_x_cen = np.sum(co_mesh_x, axis = 0)/4
+co_new_mesh_y_cen = np.sum(co_mesh_y, axis = 0)/4
+
+al_new_mesh_sep_out_target = np.zeros(2)
+al_new_mesh_sep_out_target[0] = al_new_mesh_x[1,18,96]
+al_new_mesh_sep_out_target[1] = al_new_mesh_y[1,18,96]
+al_new_mesh_x_cen = np.sum(al_new_mesh_x, axis = 0)/4
+al_new_mesh_y_cen = np.sum(al_new_mesh_y, axis = 0)/4
+
+newSAS_sep_out_target = np.zeros(2)
+newSAS_sep_out_target[0] = newSAS_mesh_x[1,18,96]
+newSAS_sep_out_target[1] = newSAS_mesh_y[1,18,96]
+newSAS_mesh_x_cen = np.sum(newSAS_mesh_x, axis = 0)/4
+newSAS_mesh_y_cen = np.sum(newSAS_mesh_y, axis = 0)/4
+
+
+
+X_point = []
+
+
+
+co_sep_out_target_dist = np.zeros(ny+2)
+co_sep_in_target_dist = np.zeros(ny+2)     
+co_sep_out_midplane_dist = np.zeros(ny+2)
+co_sep_in_midplane_dist = np.zeros(ny+2)
+
+co_new_mesh_sep_out_midplane_dist = np.zeros(ny+2)
+co_new_mesh_sep_in_midplane_dist = np.zeros(ny+2)
+co_new_mesh_sep_out_target_dist = np.zeros(ny+2)
+co_new_mesh_sep_in_target_dist = np.zeros(ny+2)
+
+al_sep_out_midplane_dist = np.zeros(ny+2)
+al_sep_in_midplane_dist = np.zeros(ny+2)
+al_sep_out_target_dist = np.zeros(ny+2)
+al_sep_in_target_dist = np.zeros(ny+2)
+
+
+al_new_mesh_sep_out_midplane_dist = np.zeros(ny+2)
+al_new_mesh_sep_in_midplane_dist = np.zeros(ny+2)
+al_new_mesh_sep_out_target_dist = np.zeros(ny+2)
+al_new_mesh_sep_in_target_dist = np.zeros(ny+2)
+
+newSAS_sep_out_midplane_dist = np.zeros(ny+2)
+newSAS_sep_in_midplane_dist = np.zeros(ny+2)
+newSAS_sep_out_target_dist = np.zeros(ny+2)
+newSAS_sep_in_target_dist = np.zeros(ny+2)
+
+
+conv_Xpt_dist = np.zeros(20)
+al_Xpt_dist = np.zeros(20)
+
+
+
+
+
+
+for i in range(ny+2):
+    co_sep_out_midplane_dist[i] = co_distance_point([52,i,1],[52,18,1])
+    co_sep_in_midplane_dist[i]  = co_distance_point([29,i,1],[29,18,1])
+    co_sep_out_target_dist[i] = co_distance_point([96,i,1],[96,18,1])
+    co_sep_in_target_dist[i] = co_distance_point([1,i,1],[1,18,1])
+
+
+    co_new_mesh_sep_out_midplane_dist[i] = co_new_mesh_distance_point([52,i,1],[52,18,1])
+    co_new_mesh_sep_in_midplane_dist[i]  = co_new_mesh_distance_point([29,i,1],[29,18,1])
+    co_new_mesh_sep_out_target_dist[i] = co_new_mesh_distance_point([96,i,1],[96,18,1])
+    co_new_mesh_sep_in_target_dist[i]  = co_new_mesh_distance_point([1,i,1],[1,18,1])
+
+    al_sep_out_midplane_dist[i] = al_distance_point([52,i,1],[52,18,1])
+    al_sep_in_midplane_dist[i]  = al_distance_point([29,i,1],[29,18,1])
+    al_sep_out_target_dist[i] = al_distance_point([96,i,1],[96,18,1])
+    al_sep_in_target_dist[i]  = al_distance_point([1,i,1],[1,18,1])
+
+
+    al_new_mesh_sep_out_midplane_dist[i] = al_new_mesh_distance_point([52,i,1],[52,18,1])
+    al_new_mesh_sep_in_midplane_dist[i]  = al_new_mesh_distance_point([29,i,1],[29,18,1])
+    al_new_mesh_sep_out_target_dist[i] = al_new_mesh_distance_point([96,i,1],[96,18,1])
+    al_new_mesh_sep_in_target_dist[i]  = al_new_mesh_distance_point([1,i,1],[1,18,1])
+    
+
+    newSAS_sep_out_midplane_dist[i] = newSAS_distance_point([52,i,1],[52,18,1])
+    newSAS_sep_in_midplane_dist[i]  = newSAS_distance_point([29,i,1],[29,18,1])
+    newSAS_sep_out_target_dist[i] = newSAS_distance_point([96,i,1],[96,18,1])
+    newSAS_sep_in_target_dist[i]  = newSAS_distance_point([1,i,1],[1,18,1])
+    
+    if i<18:
+        co_sep_out_midplane_dist[i] = -co_sep_out_midplane_dist[i]
+        co_sep_in_midplane_dist[i]  = -co_sep_in_midplane_dist[i]
+        co_sep_out_target_dist[i] = -co_sep_out_target_dist[i]
+        co_sep_in_target_dist[i] = -co_sep_in_target_dist[i]
+
+        co_new_mesh_sep_out_midplane_dist[i] = -co_new_mesh_sep_out_midplane_dist[i]
+        co_new_mesh_sep_in_midplane_dist[i]  = -co_new_mesh_sep_in_midplane_dist[i]
+        co_new_mesh_sep_out_target_dist[i] = -co_new_mesh_sep_out_target_dist[i]
+        co_new_mesh_sep_in_target_dist[i] = -co_new_mesh_sep_in_target_dist[i]
+
+        
+        al_sep_out_midplane_dist[i] = -al_sep_out_midplane_dist[i]
+        al_sep_in_midplane_dist[i] = -al_sep_in_midplane_dist[i]
+        al_sep_out_target_dist[i] = -al_sep_out_target_dist[i]
+        al_sep_in_target_dist[i] = -al_sep_in_target_dist[i]
+        
+        al_new_mesh_sep_out_midplane_dist[i] = -al_new_mesh_sep_out_midplane_dist[i]
+        al_new_mesh_sep_in_midplane_dist[i] = -al_new_mesh_sep_in_midplane_dist[i]
+        al_new_mesh_sep_out_target_dist[i] = -al_new_mesh_sep_out_target_dist[i]
+        al_new_mesh_sep_in_target_dist[i] = -al_new_mesh_sep_in_target_dist[i]
+
+        
+        newSAS_sep_out_midplane_dist[i] = -newSAS_sep_out_midplane_dist[i]
+        newSAS_sep_in_midplane_dist[i] = -newSAS_sep_in_midplane_dist[i]
+        newSAS_sep_out_target_dist[i] = -newSAS_sep_out_target_dist[i]
+        newSAS_sep_in_target_dist[i] = -newSAS_sep_in_target_dist[i]
+        
+
+
+
+
+    
+for i in range(20):
+    conv_Xpt_dist[i] = co_distance_center([78,18],[78+i,18])
+
+    
+    
+for i in range(20):
+    al_Xpt_dist[i] = al_distance_center([78,18],[78+i,18])
+
+
+def con_outer_target_dist():
+    return(co_sep_in_target_dist)
+def con_inner_target_dist():
+    return(co_sep_out_target_dist)
+
+def SAS_inner_target_dist():
+    retrun(al_sep_in_target_dist)
+def SAS_outer_target_dist():
+    retrun(al_sep_out_target_dist)
+    
+def con_newmesh_outer_target_dist():
+    return(co_new_mesh_sep_out_target_dist)
+def con_newmesh_innerer_target_dist():
+    return(co_new_mesh_sep_in_target_dist)
+
+def SAS_newmesh_outer_target_dist():
+    return(al_new_mesh_sep_out_target_dist)
+def SAS_newmesh_inner_target_dist():
+    return(al_new_mesh_sep_in_target_dist)
+
+
+def newSAS_outer_target_dist():
+    return(newSAS_sep_out_target_dist)
+def newSAS_inner_target_dist():
+    return(newSAS_sep_in_target_dist)
---
+++ data_reading.py
@@ -0,0 +1,173 @@
+import numpy as np
+
+
+fname_list = list()
+fname_list.append("b2fplasmf")
+
+
+
+data_list = list()
+data_list.append("fht")
+data_list.append("fhi")
+data_list.append("fhe")
+data_list.append("rqrad")
+data_list.append("na")
+data_list.append("ne")
+data_list.append("ti")
+data_list.append("te")
+
+data_list.append("rrana")
+data_list.append("rrahi")
+data_list.append("rcxhi")
+data_list.append("rsana")
+data_list.append("rsahi")
+data_list.append("po")
+
+
+
+nx = 96
+ny = 36
+ns = 20
+
+
+
+
+
+for date_ran in range(len(fname_list)):
+    dum_name = fname_list[date_ran]
+    fname = "./%s.txt" %dum_name
+    with open(fname,'r') as f:
+        line = f.readlines()    
+    a=type(line)
+    k = len(line)
+    data_array = np.array(line)
+    full_length = np.size(data_array)
+    
+
+        
+    for var_ran in range(len(data_list)):
+        data_name = data_list[var_ran]
+
+        print(line[2])
+        mesh = line[2].split()
+        nx = int(mesh[0])
+        ny = int(mesh[1])
+        ns = int(mesh[2])
+
+        print(line[2])
+
+        raw_data = np.empty((full_length,6))
+
+        for i in range(0, full_length):
+
+            dum = line[i].split()
+            dum2 = len(dum)
+
+            for j in range(6):
+                if j+1<=len(dum):
+
+                    dum3 = dum[j]
+
+                    try:
+                        raw_data[i,j] = dum3
+                    except ValueError as m:
+                        raw_data[i,j] = 0
+                    #print(dum3)
+                elif j+1>len(dum):
+                    continue
+
+
+
+        for i in range(0, full_length):  
+            dum = line[i].split()
+            dum2 = len(dum)
+            for j in range(6):
+                if j+1<=len(dum):
+                    dum3 = dum[j]
+                    try:
+                        raw_data[i,j] = dum3
+                    except ValueError as m:
+                        raw_data[i,j] = 0
+                        #print(line[i])
+                        break;
+                elif j+1>len(dum):
+                    continue
+
+        check_array = np.empty((1,6))
+        for ch in range(full_length):
+            if data_name in line[ch]:
+                if line[ch].split()[3]==data_name:
+                    position_start = ch+1
+                    break;
+                else:
+                    continue
+            else:
+                continue
+
+        for ch2 in range(position_start+1,full_length):
+            try:
+                check_cf = line[ch2].split()
+                check_er = float(check_cf[0])
+            except ValueError as m:
+                position_end = ch2-1
+        #        print(ch2)
+                break;
+
+        #print(line[position_end])
+        check_length = line[position_start-1].split()
+        tot_len =int(check_length[2])
+
+
+
+
+        line[250193].split()[3] == data_name
+
+
+        selected_length = position_end-position_start
+        selected_data = np.zeros((selected_length+1,6))
+
+        for i in range(position_start, position_end+1):
+            dum = line[i].split()
+            #print(line[i])
+            dum2 = len(dum)
+            for j in range(dum2):
+                dum3 = dum[j]
+                selected_data[i-position_start,j] = dum3    
+
+        #    for j in range(6):
+        #        if j+1<=len(dum):
+        #            dum3 = dum[j]
+        #            try:
+        #                selected_data[i-position_start,j] = dum3
+        #            except ValueError as m:
+        #                selected_data[i-position_start,j] = 0.0
+        #            #print(dum3)
+        #        elif j+1>len(dum):
+        #            continue
+
+
+
+        numerical_len = np.shape(selected_data)[0]*np.shape(selected_data)[1]
+
+
+
+
+        np.shape(selected_data)
+        selected_data_merged = selected_data.reshape(numerical_len,1)
+        selected_data_merged_deleted = selected_data_merged[:tot_len,0]
+
+
+        if check_length[2] == '3724':
+            selected_data_final = selected_data_merged_deleted.reshape(ny+2,nx+2)
+        elif check_length[2]== '7448':
+            selected_data_final = selected_data_merged_deleted.reshape(2, ny+2,nx+2)
+        else:
+            selected_data_final = selected_data_merged_deleted.reshape(ns, ny+2,nx+2)
+
+        np.save("./%s_%s" %(data_name,dum_name), selected_data_final)
+        print("./%s_%s" %(data_name,fname))
+
+        #selected_data_final = selected_data_merged_deleted.reshape(ns, ny+2,nx+2)
+
+        x_axis = np.linspace(1,98,98)
+        y_axis = np.linspace(1,38,38)
